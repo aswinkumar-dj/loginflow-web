a83b4dbc230a9de2d9434ada6efd155f3f261b04
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-12-06 20:04:19 +0530

    Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <nav className="w-full border-b border-white py-6 px-8 flex justify-between">
+      <h1 className="text-white font-extrabold">Automate</h1>
+      <button className="bg-white px-2 py-1">Logout</button>
+    </nav>
+  );
+};
+
+export default page;
